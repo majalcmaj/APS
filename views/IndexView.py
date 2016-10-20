22328b83077c7ae3a4a27f0f8acc5ee7f77fbcc5
majalcmaj <majalcmaj@gmail.com>
2016-10-21 00:09:07 +0200

    Configured bootstrap front-end framework

--- views/IndexView.py
+++ views/IndexView.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 
+from acquisition_presentation_server.models import Client
+
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'acquisition_presentation_server/index.html')
+        clients = Client.objects.all()
+        context = {"clients":clients}
+        return render(request, 'acquisition_presentation_server/index.html', context)
