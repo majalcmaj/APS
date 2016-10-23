7bddd0aeb4b1f6251dafbf04e46b04aafd7b0bb5
majalcmaj <majalcmaj@gmail.com>
2016-10-23 14:39:05 +0200

    Diagrams with mock data displayed well

--- views/IndexView.py
+++ views/IndexView.py
@@ -1,10 +1,20 @@
+import json
+
 from django.shortcuts import render
 from django.views import View
 
+from acquisition_presentation_server.common.DataProvider import get_client_data
 from acquisition_presentation_server.models import Client
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        clients = Client.objects.all()
-        context = {"clients":clients}
+        clients_data = []
+        for client in Client.objects.all() :
+            clients_data.append(
+                {
+                    "client" : client,
+                    "monitoring_data" : json.dumps(get_client_data())
+                }
+            )
+        context = {"clients_data":clients_data}
         return render(request, 'acquisition_presentation_server/index.html', context)
