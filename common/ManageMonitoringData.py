d7dc4ea492ece9d3f0fe9539d7184acc8ff48188
Artur <arturz24@o2.pl>
2016-10-27 00:36:47 +0200

    adds receiving data functionality

--- common/ManageMonitoringData.py
+++ common/ManageMonitoringData.py
@@ -1,5 +1,13 @@
+from acquisition_presentation_server.models import Client
+from acquisition_presentation_server.common.RRDtoolManager import RRDtoolManager
+
+
 class ManageMonitoringData:
-    #TODO Tutaj powinno być obsługiwane rrdtool
+
     @staticmethod
-    def process_data(ip_address, json_data):
-        print(json_data)
+    def process_data(ip_address, records):
+        print(ip_address,records)
+        client = Client.objects.get(ip_address=ip_address)
+        RRDtoolManager(client).update_rrd(records)
+
+
