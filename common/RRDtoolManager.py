882b5454feae7bd2857bdd1aa6c1dc925699de66
Artur <arturz24@o2.pl>
2016-10-30 13:48:41 +0100

    Removed unnecessary rrd info command execution in fetch and update

--- common/RRDtoolManager.py
+++ common/RRDtoolManager.py
@@ -5,11 +5,10 @@ from acquisition_presentation_server import settings
 
 
 class RRDtoolManager:
-
     def __init__(self, client):
         self._path = settings.RRD_DATABASE_DIRECTORY
         self._hostname = client.hostname
-        self._monitored_properties = {property.name:property.type for property in client.monitored_properties.all()}
+        self._monitored_properties = {property.name: property.type for property in client.monitored_properties.all()}
         self._probing_interval = client.probing_interval
 
     def create_rrd(self):
@@ -20,7 +19,7 @@ class RRDtoolManager:
                 monitoring_parameters.append("DS:" + k + ":GAUGE:" + str(2 * self._probing_interval) + ":U:U")
 
         rrd_archives = []
-        rrd_archives.append("RRA:LAST:0.5:1:120")
+        rrd_archives.append("RRA:AVERAGE:0.5:1:120")
 
         command = []
         command.append(rrd_database_name)
@@ -38,19 +37,24 @@ class RRDtoolManager:
 
     def update_rrd(self, records):
         rrd_database_name = self._get_rrd_abs_path()
+        record_string = str(int(time.time()))
+        template_string = ""
+        for k, v in records.items():
+            template_string += ":" + k
+            record_string += ":" + v
+
+        rrdtool.update(rrd_database_name, '--template', template_string[1:], record_string)
+
+    def update_rrd_with_order(self, records):
+        rrd_database_name = self._get_rrd_abs_path()
         order = self._retreive_order_of_rows(rrd_database_name)
         record_string = str(int(time.time()))
         for record in order:
             record_string += ":" + records[record]
-
-        # print(record_string)
         rrdtool.update(rrd_database_name, record_string)
 
-    # TODO Nie da się inaczej, niż czytać plik
-    # dla paru klientów ok, ale jak z 50 co sekundę to może być wąskie gardło
-
-
-    def _retreive_order_of_rows(self, path):
+    @staticmethod
+    def _retreive_order_of_rows(path):
         params = []
         for k, v in rrdtool.info(path).items():
             if k.find('].index') != -1:
@@ -61,17 +65,16 @@ class RRDtoolManager:
     def _get_rrd_abs_path(self):
         return self._path + "/" + self._hostname + ".rrd"
 
-    # TODO Czy da się pobierać tylko jedną daną?
     def fetch_data(self, time_period):
         rrd_database_name = self._get_rrd_abs_path()
 
         current_time = int(time.time())
-        records = rrdtool.fetch(rrd_database_name, 'LAST', '--start', str(current_time-time_period), '--end',
+        records = rrdtool.fetch(rrd_database_name, 'LAST', '--start', str(current_time - time_period), '--end',
                                 str(current_time))
         result = {}
-        result['unix_time'] = [current_time-time_period + i for i in range(1, len(records[2]) + 1)]
+        result['unix_time'] = [current_time - time_period + i for i in range(1, len(records[2]) + 1)]
         data = result['data'] = {}
-        order = self._retreive_order_of_rows(rrd_database_name)
+        order = records[1]
         for i in range(0, len(order)):
             data[order[i]] = [value[i] for value in records[2]]
         return result
